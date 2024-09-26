804ee824554fc1a29a830a1efc2cf48a69432b9e
pavelvasilev <vasilievpavel4@gmail.com>
2024-09-26 17:12:56 +0500

    fast version

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import re
-from wiki import get_first_sentence_and_link
+from wiki import *
 
 references = {}
 
-def replace_words_with_links(text):
+def replace_words_with_links(text, word_data):
     words_regex = r'(?<!\\)\b\w+\b'
 
     def replacer(match):
         word = match.group(0)
-        w = get_first_sentence_and_link(word)
-        if w:
-            sentence, link = w
+        if word in word_data and word_data[word]:
+            sentence, link = word_data[word]
             ref_key = word.replace(" ", "_")
             references[ref_key] = sentence
             references[word] = sentence
             return f"\\href{{{link}}}{{{word}}} (\\cite{{{ref_key}}})"
+        return word
 
     updated_text = re.sub(words_regex, replacer, text)
     return updated_text
 
-def process_sexy_abstract_block(latex_content):
+
+async def fetch_references(words):
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_first_sentence_and_link_async(session, word) for word in words]
+        results = await asyncio.gather(*tasks)
+        return dict(zip(words, results))
+
+
+async def process_sexy_abstract_block_async(latex_content):
     pattern = re.compile(r'(?s)(% SEXY_ABSTRACT_BEGIN.*?% SEXY_ABSTRACT_END)')
 
     def process_block(match):
@@ -27,12 +35,23 @@ def process_sexy_abstract_block(latex_content):
         abstract_content = re.search(r'(?<=% SEXY_ABSTRACT_BEGIN)(.*?)(?=% SEXY_ABSTRACT_END)', block_text, re.DOTALL)
         if abstract_content:
             original_text = abstract_content.group(1)
-            updated_text = replace_words_with_links(original_text)
-            return block_text.replace(original_text, updated_text)
-        return block_text
+            words = re.findall(r'(?<!\\)\b\w+\b', original_text)
+            return original_text, block_text, words
+        return None, block_text, []
 
-    updated_content = re.sub(pattern, process_block, latex_content)
+    matches = [process_block(match) for match in pattern.finditer(latex_content)]
+
+    all_words = {word for _, _, words in matches for word in words}
+    word_data = await fetch_references(all_words)
+
+    updated_content = latex_content
+    for original_text, block_text, words in matches:
+        if original_text:
+            updated_text = replace_words_with_links(original_text, word_data)
+            updated_content = updated_content.replace(original_text, updated_text)
+
     return updated_content
+
 
 def add_references(latex_content):
     pattern = re.compile(r'(?s)(% SEXY_REFERENCES_BEGIN.*?% SEXY_REFERENCES_END)')
@@ -41,19 +60,24 @@ def add_references(latex_content):
         block_text = match.group(1)
         updated_text = ''
         for reference in references:
-            updated_text += f'\\bibitem{{{reference}}} {references[reference]}\n'
+            updated_text += f'\\bibitem{{{reference}}} \\newblock {references[reference]}\n'
         return block_text.replace(block_text, f'% SEXY_REFERENCES_BEGIN\n{updated_text}% SEXY_REFERENCES_END')
 
     updated_content = re.sub(pattern, process_block, latex_content)
     return updated_content
 
-def process_latex_file(input_file, output_file):
+
+# Основная функция для обработки файла
+async def process_latex_file_async(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    updated_content = process_sexy_abstract_block(content)
+    updated_content = await process_sexy_abstract_block_async(content)
     updated_content = add_references(updated_content)
+
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(updated_content)
 
-process_latex_file('doc.tex', 'article_with_links.tex')
+
+# Запуск асинхронной обработки
+asyncio.run(process_latex_file_async('doc.tex', 'article_with_links.tex'))
--- wiki.py
+++ wiki.py
@@ -1,48 +1,43 @@
-import requests
+import asyncio
+import aiohttp
 
-
-def get_first_sentence_and_link(keyword):
+async def get_first_sentence_and_link_async(session, keyword):
     url = "https://en.wikipedia.org/w/api.php"
 
-    # Параметры для основного запроса
     params = {
         "action": "query",
         "format": "json",
         "titles": keyword,
         "prop": "extracts|info",
-        "exintro": True,
-        "explaintext": True,
-        "redirects": 1,
+        "exintro": "1",
+        "explaintext": "1",
+        "redirects": "1",
         "inprop": "url"
     }
 
-    response = requests.get(url, params=params)
+    async with session.get(url, params=params) as response:
+        if response.status == 200:
+            data = await response.json()
+            pages = data['query']['pages']
 
-    if response.status_code == 200:
-        data = response.json()
-        pages = data['query']['pages']
-
-        for page_id, page in pages.items():
-            if 'extract' in page and 'may refer to' not in page['extract']:
-                extract = page['extract']
-                first_sentence = extract.split('\n')[0] + '.'
-                article_title = page['title']
-                article_url = f"https://en.wikipedia.org/wiki/{article_title.replace(' ', '_')}"
-
-                print(keyword,  article_url)
-                return first_sentence, article_url
-
-            # Если статья не найдена, выполняем поиск
-            else:
-                related_keyword = get_related_articles(keyword)
-                if related_keyword:
-                    return get_first_sentence_and_link(related_keyword)
-
-    else:
+            for page_id, page in pages.items():
+                if 'extract' in page and 'may refer to' not in page['extract']:
+                    extract = page['extract']
+                    first_sentence = '.'.join(extract.split('.')[:2]) + '.'
+                    article_title = page['title']
+                    article_url = f"https://en.wikipedia.org/wiki/{article_title.replace(' ', '_')}"
+                    print(keyword, article_url)
+                    return first_sentence, article_url
+                else:
+                    related_keyword = await get_related_articles_async(session, keyword)
+                    if keyword == related_keyword:
+                        return None
+                    if related_keyword:
+                        return await get_first_sentence_and_link_async(session, related_keyword)
         return None
 
 
-def get_related_articles(keyword):
+async def get_related_articles_async(session, keyword):
     url = "https://en.wikipedia.org/w/api.php"
 
     params = {
@@ -53,16 +48,10 @@ def get_related_articles(keyword):
         "format": "json"
     }
 
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        if len(data[1]) > 0:
-            related_title = data[1][1 if len(data[1]) > 0 else 0]
-            return related_title
-
-    return None
-#
-# first_sentence, article_link = get_first_sentence_and_link('Python')
-# print("First sentence:", first_sentence)
-# print("Article link:", article_link)
+    async with session.get(url, params=params) as response:
+        if response.status == 200:
+            data = await response.json()
+            if len(data[1]) > 0:
+                related_title = data[1][1 if len(data[1]) > 1 else 0]
+                return related_title
+        return None
